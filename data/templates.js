0150c67e2fad30db2a7cf32b1e95d3a8d2377479
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-03-14 12:06:46 -0700

    adding help (?) in dashboard. kewl

--- data/templates.js
+++ data/templates.js
@@ -245,6 +245,49 @@ _templates['feedback/thanks'] = '<h1>Thanks for getting in touch</h1>\
 </div>\
 ';
 
+_templates['help/index'] = '<h1>Quick help</h1>\
+<div class="help content">\
+	<h2>Keyboard shortcuts</h2>\
+	<table class="shortcuts">\
+		<tr>\
+			<td><kbd>a</kbd></td>\
+			<td>Add a new note</td>\
+		</tr>\
+		<tr>\
+			<td><kbd>b</kbd></td>\
+			<td>Add a new board</td>\
+		</tr>\
+		<tr>\
+			<td><kbd>enter</kbd></td>\
+			<td>Open current note</td>\
+		</tr>\
+		<tr>\
+			<td><kbd>e</kbd></td>\
+			<td>Edit current note</td>\
+		</tr>\
+		<tr>\
+			<td><kbd>delete</kbd></td>\
+			<td>Delete current note</td>\
+		</tr>\
+		<tr>\
+			<td><kbd>/</kbd></td>\
+			<td>Search notes</td>\
+		</tr>\
+		<tr>\
+			<td><kbd>x</kbd></td>\
+			<td>Clear all current filters (show all notes in the board)</td>\
+		</tr>\
+		<tr>\
+			<td><kbd>?</kbd></td>\
+			<td>Show this help</td>\
+		</tr>\
+	</table>\
+	<p>\
+		If you are having trouble using Turtl, please email us at <code>info@turtl.it</code>.\
+	</p>\
+</div>\
+';
+
 _templates['invites/board'] = '<div class="invite">\
 \
 	<form class="standard-form">\
